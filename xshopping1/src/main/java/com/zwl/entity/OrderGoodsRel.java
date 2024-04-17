12fde48117f87c7254a84f9729141bb9b4966790
zhongsun <361651398@qq.com>
2024-04-17 15:32:13 +0800

    第一次提交

--- xshopping1/src/main/java/com/zwl/entity/OrderGoodsRel.java
+++ xshopping1/src/main/java/com/zwl/entity/OrderGoodsRel.java
@@ -4,34 +4,25 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-/**
- *   订单商品关系映射表
- */
+//订单商品关系映射表
 @Table(name = "order_goods_rel")
 @Data
 public class OrderGoodsRel {
-    /**
-     * 自增id
-     */
+
+    //自增ID
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /**
-     *   订单ID
-     */
+    //订单ID
     @Column(name = "orderId")
     private Long orderid;
 
-    /**
-     *   所属商品
-     */
+    //所属商品
     @Column(name = "goodsId")
     private Long goodsid;
 
-    /**
-     *   商品数量
-     */
+    //商品数量
     @Column(name = "count")
     private Integer count;
 
